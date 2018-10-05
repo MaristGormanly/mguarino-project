cc737b7a78bcf79b1f7ff1ceddb37fae9f8087de
mguarino1 <31972929+mguarino1@users.noreply.github.com>
2018-10-05 18:14:40 -0400

    Final Commit hotfix#2

--- client/public/js/mainmenu.js
+++ client/public/js/mainmenu.js
@@ -1,9 +1,30 @@
-partyCheck = false;
+var interval = null;
+partyTime = false;
 document.addEventListener("keydown", function(event) {
 	if(event.keyCode == 51) {
 		window.location = "topten"
 	} else if(event.keyCode == 49) {
 		window.location = "trail"
+	} else if(event.keyCode == 53 && partyTime == false) {
+		party();
+		partyTime = true;
+	} else if(event.keyCode == 53 && partyTime == true) {
+		partyStop();
+		partyTime = false;
 	}
 })
-
+function party() {
+	alert("It's party time. (WARNING: FLASHING LIGHTS)");
+	var counter = 1;
+	interval = setInterval(function() {
+		var colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+		document.body.style.background = colors[counter%colors.length];
+		counter++;
+		console.log(counter + colors[counter%colors.length])
+	}, 100);
+}
+function partyStop() {
+	alert("Party's over.");
+	clearInterval(interval);
+	document.body.style.background = "black";
+}
