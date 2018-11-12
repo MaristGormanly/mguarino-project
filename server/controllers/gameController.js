f2cd31c7e55ea12bdc54f52a6e6855c74fac9852
MIke <mguarino223@gmail.com>
2018-11-11 23:21:40 -0500

    Finished most gameController function

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -15,7 +15,46 @@ exports.updateGameData = function(req, res) {
     exports.currentData.currentWeather = weather.getRandomWeather();
     
     exports.currentData.daysOnTrail++;
-    exports.currentData.milesTraveled += Math.floor(exports.currentData.currentPace.miles * exports.currentData.currentWeather.miles); 
+    exports.currentData.milesTraveled += Math.floor(exports.currentData.currentPace.miles * exports.currentData.currentWeather.miles);
+    exports.currentData.groupHealth += exports.currentData.currentWeather.health;
+    exports.currentData.groupHealth += exports.currentData.currentPace.health;
+    if(exports.currentData.daysOnTrail >= 45) {
+        exports.currentData.messages = "ya took too long and now yer ded";
+        exports.currentData.groupHealth = 0;
+        exports.currentData.playerStatus = [true, true, true, true, true];
+    }
+    if(exports.currentData.milesTraveled >= 500) {
+        exports.currentData.messages = "wow, u really went and did it";
+    }
+    if(exports.currentData.groupHealth > 100) {
+        exports.currentData.groupHealth = 100;
+    }
+
+    var deathRoll = Math.floor(Math.random() * 100) + 1;
+    if(exports.currentData.groupHealth <= 0) {
+        exports.currentData.playerStatus = [true, true, true, true, true];
+        exports.currentData.messages = "You're all dead."
+    } else if(exports.currentData.groupHealth < 20) {
+        if(deathRoll <= 10) {
+            var i = 4;
+            while(exports.currentData.playerStatus[i] == true) {
+                i--;
+            }
+            exports.currentData.playerStatus[i] = true;
+            exports.currentData.messages = "A member of your party has died.";
+        }
+    } else if(exports.currentData.groupHealth < 50) {
+        if(deathRoll <= 3) {
+            var i = 4;
+            while(exports.currentData.playerStatus[i] == true) {
+                i--;
+            }
+            exports.currentData.playerStatus[i] = true;
+            exports.currentData.messages = "A member of your party has died.";
+        }
+    } else {
+        exports.currentData.messages = "Another day on the trail...";
+    }
     res.setHeader('Content-Type', 'application/json');
     res.send(exports.currentData);
 }
@@ -23,6 +62,13 @@ exports.updateGameData = function(req, res) {
 exports.getPace = function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send(exports.currentData.currentPace);
+}
+
+exports.setPace = function(req, res) {
+    console.log("Change pace to: " + req.body.pace);
+    exports.currentData.currentPace = pace.getAllPaces()[req.body.pace];
+    res.setHeader('Content-Type', 'application/json');
+    res.send(exports.currentData);
 }
 
 exports.resetGameData = function(req, res) {
--- server/models/weather.js
+++ server/models/weather.js
@@ -7,26 +7,31 @@ function weather(id, type, health, miles, prob) {
 }
 
 var allWeathers = []
-allWeathers.push(new weather(0, "Very Hot", -8, .7, .1));
-allWeathers.push(new weather(1, "Hot", -3, .9, .1));
-allWeathers.push(new weather(2, "Warm", 1, 1, .2));
-allWeathers.push(new weather(3, "Cool", 1, .95, .1));
-allWeathers.push(new weather(4, "Cold", -5, .8, .1));
-allWeathers.push(new weather(5, "Very Cold", -12, .7, .1));
-allWeathers.push(new weather(6, "Rain", -4, .6, .1));
-allWeathers.push(new weather(7, "Heavy Rain", -8, .4, .05));
-allWeathers.push(new weather(8, "Snow", -15, .3, .05));
-allWeathers.push(new weather(9, "Blizzard", -30, .1, .05));
-allWeathers.push(new weather(10, "Heavy Fog", -3, .5, .05));
+allWeathers.push(new weather(0, "Very Hot", -8, .7, 10));
+allWeathers.push(new weather(1, "Hot", -3, .9, 10));
+allWeathers.push(new weather(2, "Warm", 1, 1, 20));
+allWeathers.push(new weather(3, "Cool", 1, .95, 10));
+allWeathers.push(new weather(4, "Cold", -5, .8, 10));
+allWeathers.push(new weather(5, "Very Cold", -12, .7, 10));
+allWeathers.push(new weather(6, "Rain", -4, .6, 10));
+allWeathers.push(new weather(7, "Heavy Rain", -8, .4, 5));
+allWeathers.push(new weather(8, "Snow", -15, .3, 5));
+allWeathers.push(new weather(9, "Blizzard", -30, .1, 5));
+allWeathers.push(new weather(10, "Heavy Fog", -3, .5, 5));
 
 exports.getRandomWeather = function() {
-    var num = Math.random();
+    var num = (Math.floor(Math.random() * 100)) + 1;
     var probTotal = 0;
     var i = 0;
-    while(probTotal <= num) {
+    var chosen = false;
+    while(!chosen) {
         probTotal += allWeathers[i].prob;
-        i++;
-    }//while
+        if(num <= probTotal) {
+            chosen = true;
+        } else {
+            i++
+        }//if
+    } //while
     return allWeathers[i];
 }
 
